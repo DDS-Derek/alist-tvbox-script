a47d4ed606f58dc724a737c1cad07c40a8ee8134
DDSRem <73049927+DDSRem@users.noreply.github.com>
2024-08-13 15:14:34 +0800

    feat: auto restart tvbox

--- pg_auto_updater/entrypoint.sh
+++ pg_auto_updater/entrypoint.sh
@@ -12,6 +12,9 @@ function INFO() {
 while true; do
     INFO "开始更新..."
     /usr/bin/pg_auto_updater
+    if [ -n "${TVBOX_NAME}" ]; then
+        docker restart "${TVBOX_NAME}"
+    fi
     INFO "更新完成！"
     INFO "等待 $((24 * 60 * 60)) 秒后下次运行！"
     sleep "$((24 * 60 * 60))"
